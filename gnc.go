d1c6e6975bf3dc7c110b6c66be7a5a1ce2b9b023
Evan <vershun@gmail.com>
2018-06-26 00:02:26 -0700

    Add content length

--- gnc.go
+++ gnc.go
@@ -103,6 +103,7 @@ func slackbotHandler(w http.ResponseWriter, r *http.Request) {
 func (gnc *gnc) fortniteDrop(w http.ResponseWriter, r *http.Request) {
 	landingZone := randomLandingZone()
 	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Content-Length", "1000")
 	gnc.writeLandingZoneImage(landingZone, w)
 	// Save landing zone
 }
